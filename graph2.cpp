96b892d2236fc3b0a99d009ca75c2afaea44e220
Omkar Mankar <mankar.omkar2310@gmail.com>
2019-04-14 17:58:47 +0530

    Updated graph2

--- graph2.cpp
+++ graph2.cpp
@@ -1,3 +1,11 @@
+/*
+PROBLEM STATEMENT-There are flight paths between cities. If there is a flight between city A and city B then there is an edge 
+between the cities. The cost of the edge can be the time that flight takes to reach city B from A, or the amount of
+fuel used for the journey. Represent this as a graph. The node can be represented by airport name or name of the city. 
+Use adjacency list representation of the graph or use adjacency matrix representation of the graph. Justify the storage 
+representation used.(Operation to be performed adding and deleting edge, adding and deleting vertices, calculated in-degree 
+and out-degree for directed graph and degree of a node in undirected graph, Use any traversal to traverse through the graph)
+*/
 //============================================================================
 // Name        : graph2.cpp
 // Author      : OMKAR MANKAR
